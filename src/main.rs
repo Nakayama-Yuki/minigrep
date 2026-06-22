0fdaeab347af1f96e8c458ead2dadf58df872577
yuki <yuki.for.neutral@gmail.com>
2026-06-22 13:53:12 +0900

    feat: 引数を受け取るようにmain関数を変更し、検索クエリとファイルパスを表示

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,12 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    //       "{}を検索します"
+    println!("Searching for {}", query);
+    //       "ファイル{}の中で"
+    println!("In file {}", file_path);
 }
