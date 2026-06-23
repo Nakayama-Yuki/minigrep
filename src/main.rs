32b9498ed0a1f00a17192c79fc39d4912f1493fc
yuki <yuki.for.neutral@gmail.com>
2026-06-23 13:54:09 +0900

    feat: Config構造体とrun関数をlib.rsに移動し、main.rsを整理

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,23 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
 
+use minigrep::Config;
+
 fn main() {
+    // --snip--
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        //       "引数解析時の問題: {err}"
-        println!("Problem parsing arguments: {}", err);
+        println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    if let Err(e) = run(config) {
-        //　　　　""アプリケーションエラー: {e}"
-        println!("Application error: {}", e);
+    if let Err(e) = minigrep::run(config) {
+        // --snip--
+        println!("Application error: {e}");
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
     }
 }
